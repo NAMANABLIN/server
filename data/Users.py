95c42843634a8657880bfbc280b95ba8b5f3b7a3
NAMA <99044791+NAMANABLIN@users.noreply.github.com>
2023-03-11 12:58:47 +0500

    add register form

--- data/Users.py
+++ data/Users.py
@@ -1,6 +1,8 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
@@ -16,5 +18,12 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     job = orm.relationship("Job", back_populates='user')
+
     def __repr__(self):
         return f'<Colonist> {self.id} {self.surname} {self.name}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, redirect
 from data import db_session
 from data.Jobs import Job
 from data.Users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 pages = ['/', '/index',
          '/training', '/list_prof', '/answer',
-         '/auto_answer', '/login', '/table']
+         '/auto_answer', '/login', '/table', '/register']
 
 profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
          'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите', 'астрогеолог',
@@ -25,6 +27,7 @@ dict = {
     'ready': 'True'
 }
 
+
 @app.route(pages[0])
 def works_log():
     db_session.global_init('mars_explorer.db')
@@ -32,6 +35,7 @@ def works_log():
     jobs = db_sess.query(Job).all()
     users = db_sess.query(User).all()
     return render_template('jobs.html', title='Журнал работ', jobs=jobs, users=users)
+
 
 @app.route(pages[0] + '/<title>')
 @app.route(pages[1] + '/<title>')
@@ -54,13 +58,46 @@ def show_profs(list):
 def answer():
     return render_template('auto_answer.html', dict=dict)
 
+
 @app.route(pages[6])
 def login():
     return render_template('def.html', title='Аварийный доступ')
 
+
 @app.route(pages[7] + '/<sex>' + '/<age>')
 def table(sex, age):
     return render_template('table.html')
+
+
+@app.route(pages[8], methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_session.global_init('mars_explorer.db')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            email=form.email.data,
+            age=form.age.data,
+            position=form.pos.data,
+            speciality=form.spec.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 if __name__ == '__main__':
     for x in pages:
